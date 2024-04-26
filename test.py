783cc9fc81760cf6c7ae9750d9ce86291355b4e0
FlabMax <167072324+flbmax@users.noreply.github.com>
2024-04-26 15:30:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,13 @@
 import streamlit as st
 
-st.text('Fixed text')
-st.markdown('_Markdown_') # see #*
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
-
 # * optional kwarg unsafe_allow_html = True
 
 def embed_qlik_iframe(iframe_url):
     # Afficher l'iFrame dans l'application Streamlit
-    st.components.v1.iframe(iframe_url, height=600)
+    st.components.v1.iframe(iframe_url, height=600, width=400)
 
 # Lien iFrame de votre graphique Qlik Sense
 qlik_iframe_url = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu"
 # Appeler la fonction pour intégrer l'iFrame dans l'application Streamlit
 embed_qlik_iframe(qlik_iframe_url)
+
