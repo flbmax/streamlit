daeb643d98a162736ba9d398247582dacab69812
FlabMax <167072324+flbmax@users.noreply.github.com>
2024-04-26 15:33:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
-# * optional kwarg unsafe_allow_html = True
 
 def embed_qlik_iframe(iframe_url):
-    # Afficher l'iFrame dans l'application Streamlit
     st.components.v1.iframe(iframe_url, height=600, width=900)
 
-# Lien iFrame de votre graphique Qlik Sense
-qlik_iframe_url = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu"
-# Appeler la fonction pour intégrer l'iFrame dans l'application Streamlit
-embed_qlik_iframe(qlik_iframe_url)
+qlik_profiles = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu"
+qlik_teams = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu"
+embed_qlik_iframe(qlik_profiles)
+embed_qlik_iframe(qlik_teams)
+
 
