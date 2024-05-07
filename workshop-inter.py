c89d31d01fc647ee9b0fb4a6cf711d943bafe2c3
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 16:40:25 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -15,17 +15,17 @@ st.caption('by country from 01/01/2019 to today')
 
 st.subheader('a. Users')
 
+qlik_profiles = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu" style="border:none;width:100%;height:100%"
+embed_qlik_iframe(qlik_profiles)
 
 
 
 
 
 
-#qlik_profiles = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu"
 #qlik_teams = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu"
 
 
-#embed_qlik_iframe(qlik_profiles)
 #embed_qlik_iframe(qlik_teams)
 
 
