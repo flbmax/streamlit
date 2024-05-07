c492502b1c27d2d2ba82252aeb0467b4e789aac4
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 17:49:26 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -9,7 +9,7 @@ st.markdown('2024-05-21')
 st.header(':grey[1. Growth in number of users, teams & clubs]')
 st.caption('by country from 01/01/2019 to today')
 
-st.subheader(':violet[a. Users]')
+st.subheader(':green[a. Users]')
 # profiles created
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu",
         height=500,
@@ -19,14 +19,14 @@ st.caption('*using profile.default_language to determine the country')
 
 col1, col2 = st.columns(2, gap='large')
 with col1:
-    st.subheader('b. Teams')
+    st.subheader(':green[b. Teams]')
     # teams created
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu",
             height=500,
             width=650)
     st.caption('*using team.country_id to determine the country')
 with col2:
-    st.subheader('c. Clubs')
+    st.subheader(':green[c. Clubs]')
     # clubs created
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=juSbKpp&theme=horizon&opt=ctxmenu",
             height=500,
@@ -35,27 +35,27 @@ with col2:
     st.caption("Churn : le club a payé au moins une fois sur SportEasy et est maintenant en Free Plan ou Free Trial Plan")
 
 st.divider()
-st.header('2. Typology of teams by country today')
+st.header(':grey[2. Typology of teams by country today]')
 
-st.subheader('a. Type Plan, Group Type, Gender, Device')
+st.subheader(':green[a. Type Plan, Group Type, Gender, Device]')
 # trellis teams
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=SJrNx&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
 
-st.subheader('b. Sport')
+st.subheader(':green[b. Sport]')
 # sport pie chart
 components.iframe( "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu",
     height=400,
     width=1700)
 
-st.subheader('c. First Source')
+st.subheader(':green[c. First Source]')
 # first source pie chart
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=ssTyVB&theme=horizon&opt=ctxmenu",
     height=400,
     width=1700)
 
-st.subheader('d. Activation Analysis')
+st.subheader(':green[d. Activation Analysis]')
 # A2
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=MtxHs&theme=horizon&opt=ctxmenu",
         height=500,
