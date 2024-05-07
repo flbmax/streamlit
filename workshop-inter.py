368d4a8ef6e17bba2e196b7ef53b0c65eaecd183
FlabMax <167072324+flbmax@users.noreply.github.com>
2024-05-07 15:42:08 +0200

    Update workshop-inter.py

--- workshop-inter.py
+++ workshop-inter.py
@@ -4,6 +4,9 @@ import streamlit as st
 def embed_qlik_iframe(iframe_url):
     st.components.v1.iframe(iframe_url, height=600, width=900)
 
+st.title('Workshop International Team 2024-05-21')
+
+
 #qlik_profiles = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu"
 #qlik_teams = "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu"
 
