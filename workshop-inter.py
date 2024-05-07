4a720a17ef58ac5d54d198ede3f2141c69ddffa7
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 16:31:45 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="worksop-international", 
-    page_icon="sporteasy_logo.jpeg",
+    page_icon="sporteasy_logo.png",
     layout="wide")
 
 # Functions
