82c017a9255415e3e88ff1d1793e35ffddb3e7a9
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 17:41:57 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -3,13 +3,13 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="workshop-international", layout="wide")
 
-st.title(':green[Workshop International Team]')
+st.title(':rocket: :green[Workshop International Team]')
 st.markdown(':green[2024-05-21]')
 
 st.header('1. Growth in number of users, teams & clubs')
 st.caption('by country from 01/01/2019 to today')
 
-st.subheader('a. Users')
+st.subheader(':violet[a. Users]')
 # profiles created
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu",
         height=500,
