225e9f63ab9bac6a7c1e1ad3e84758999652036c
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 17:37:12 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -59,7 +59,6 @@ st.subheader('d. Activation Analysis')
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=MtxHs&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
-
-
+st.caption("We can notice a global saisonnality everery year !")
 
        
