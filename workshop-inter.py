fae86a1ad83967b878f962498e1395e0c136e937
flabmax <maxence.flaba@sporteasy.net>
2024-05-07 17:59:44 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="workshop-international", page_icon='Initials.png', layout="wide")
+st.set_page_config(page_title="workshop-international", page_icon='sporteasy_logo.png', layout="wide")
 
 st.title(':flag-eu: :blue[Workshop International Team]')
 st.markdown('2024-05-21')
