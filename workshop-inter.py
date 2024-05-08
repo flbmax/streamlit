9e213951f47ab2bb411985453c349e196b9840bb
flabmax <maxence.flaba@sporteasy.net>
2024-05-08 18:58:28 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -6,27 +6,33 @@ st.set_page_config(page_title="workshop-international", page_icon='sporteasy_log
 st.title(':flag-eu: :blue[Workshop International Team]')
 st.markdown('2024-05-21')
 
-st.header('1. Growth in number of users, teams & clubs')
+st.header('1. Growth in number of profiles, teams & clubs')
 st.caption('by country from 01/01/2019 to today')
 
-st.subheader(':green[a. Users]')
+st.subheader(':green[a. Profiles]')
 # profiles created
 components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu",
         height=500,
         width=1000)
 st.caption('*using profile.default_language to determine the country')
 
+st.subheader(':green[b. MAU]')
+# MAU
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=mzCzqu&theme=horizon&opt=ctxmenu",
+        height=500,
+        width=1000)
+st.caption('*using profile.default_language to determine the country')
 
 col1, col2 = st.columns(2, gap='large')
 with col1:
-    st.subheader(':green[b. Teams]')
+    st.subheader(':green[c. Teams]')
     # teams created
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=kxPqHuT&theme=horizon&opt=ctxmenu",
             height=500,
             width=650)
     st.caption('*using team.country_id to determine the country')
 with col2:
-    st.subheader(':green[c. Clubs]')
+    st.subheader(':green[d. Clubs]')
     # clubs created
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=juSbKpp&theme=horizon&opt=ctxmenu",
             height=500,
@@ -108,9 +114,10 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-
 
 st.subheader(':green[f. Health Score]')
 # clubs health score
-#components.iframe(,
-#    height=500,
-#    width=1000)
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=thLrd&theme=horizon&opt=ctxmenu",
+    height=500,
+    width=1000)
+st.write("The French trend is better : high-rated clubs are paying more")
 
 st.divider()
 st.header('4. Churn Analysis')
