d2f0af38b9efe5736c62fdf83300885c3370f931
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 16:59:38 +0200

    test

--- pages/3_3._Typology_of_clubs_by_country_today.py
+++ pages/3_3._Typology_of_clubs_by_country_today.py
@@ -3,6 +3,9 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="workshop-international", page_icon='sporteasy_logo.png', layout="wide")
 
+st.title(':flag-eu: :blue[Workshop International Team]')
+st.markdown('2024-05-21')
+
 st.header('3. Typology of clubs by country today')
 
 st.subheader(':green[a. Type Plan, Lifetime]')
