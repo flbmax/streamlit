81019d752dd8020e189bdeae87c7bf34154253eb
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 17:57:54 +0200

    churn

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -9,9 +9,9 @@ st.markdown('2024-05-21')
 st.header('4. Churn Analysis')
 st.caption('by country from 01/01/2019 to today')
 
-st.subheader(':green[a. Numbers]')
+st.subheader(':green[a. Targets & Results]')
 with st.container():
-    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=hJUtP&theme=horizon&opt=ctxmenu&select=2-workshop::%3DIf(Match(club.country_id%2C'ES'%2C'IT'%2C'GB'%2C'NL'%2C'PT')%2Cclub.country_id),ES,GB,IT,NL,PT",
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=hJUtP&theme=horizon&opt=ctxmenu",
         height=50,
         width=400)
     # results
@@ -24,9 +24,7 @@ with st.container():
         width=650)
 
 
-
-
-st.subheader(':green[b. 2024 Target]')
+st.subheader(':green[b. 2024 Objective]')
 # target of renewal
 with st.container():
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=LHhEnK&theme=horizon&opt=ctxmenu",
