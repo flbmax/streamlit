5ab1ec74c69a3ad230c27c9ab2ccf9f64149faa8
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 14:47:27 +0200

    form

--- workshop-inter.py
+++ workshop-inter.py
@@ -142,9 +142,12 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-
 
 st.subheader(':green[c. Churn Analysis]')
 # churn the last 5 years
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=LHhEnK&theme=horizon&opt=ctxmenu",
-        height=100,
-        width=300)
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=GQmHumj&theme=horizon&opt=ctxmenu&select=KPI%20AM::club.country_id,ES,GB,IT,NL,PT",
-        height=500,
-        width=1000)
+col3, col4 = st.columns(2, gap='large')
+with col3:
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=LHhEnK&theme=horizon&opt=ctxmenu",
+            height=1000,
+            width=300)
+with col4:
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=GQmHumj&theme=horizon&opt=ctxmenu&select=KPI%20AM::club.country_id,ES,GB,IT,NL,PT",
+            height=500,
+            width=1000)
