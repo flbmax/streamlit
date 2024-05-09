84029cfa7b5e57a44c1de7b2e67e96226e267a1b
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 17:55:53 +0200

    churn

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -16,11 +16,11 @@ with st.container():
         width=400)
     # results
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=Btfjt&theme=horizon&opt=ctxmenu&select=2-workshop::%3DIf(Match(club.country_id%2C'ES'%2C'IT'%2C'GB'%2C'NL'%2C'PT')%2Cclub.country_id),ES,GB,IT,NL,PT",
-        height=400,
+        height=300,
         width=650)
     # target
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=tNSLpW&theme=horizon&opt=ctxmenu&select=2-workshop::%3DIf(Match(club.country_id%2C'ES'%2C'IT'%2C'GB'%2C'NL'%2C'PT')%2Cclub.country_id),ES,GB,IT,NL,PT",
-        height=400,
+        height=300,
         width=650)
 
 
