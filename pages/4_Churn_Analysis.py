174f619bd16ef6bf6d32fb1cf3de5689b02e7b9d
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 17:25:49 +0200

    churn

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -9,12 +9,21 @@ st.markdown('2024-05-21')
 st.header('4. Churn Analysis')
 st.caption('by country from 01/01/2019 to today')
 
-col3, col4 = st.columns(2, gap='small')
-with col3:
+with st.container():
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=LHhEnK&theme=horizon&opt=ctxmenu",
             height=500,
             width=100)
-with col4:
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=GQmHumj&theme=horizon&opt=ctxmenu&select=KPI%20AM::club.country_id,ES,GB,IT,NL,PT",
             height=500,
             width=1000)
+
+
+#col3, col4 = st.columns(2, gap='small')
+#with col3:
+##    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=LHhEnK&theme=horizon&opt=ctxmenu",
+ #           height=500,
+ #           width=100)
+#with col4:
+#    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=GQmHumj&theme=horizon&opt=ctxmenu&select=KPI%20AM::club.country_id,ES,GB,IT,NL,PT",
+#            height=500,
+#            width=1000)
