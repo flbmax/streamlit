9c58528f30f94d7fe40fac174eb41de4ae714afc
flabmax <maxence.flaba@sporteasy.net>
2024-05-09 17:00:07 +0200

    test

--- pages/4_4._Churn_Analysis.py
+++ pages/4_4._Churn_Analysis.py
@@ -3,5 +3,8 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="workshop-international", page_icon='sporteasy_logo.png', layout="wide")
 
+st.title(':flag-eu: :blue[Workshop International Team]')
+st.markdown('2024-05-21')
+
 st.header('4. Churn Analysis')
 st.caption('by country from 01/01/2019 to today')
