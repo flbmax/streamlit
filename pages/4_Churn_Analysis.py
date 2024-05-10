4bac226323eb899f87dbfdfeee19373944d66e53
flabmax <maxence.flaba@sporteasy.net>
2024-05-10 14:12:17 +0200

    button

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -107,14 +107,14 @@ with st.container():
                 width=100)
 
 multi = '''
-We can notice the high number of churn clubs and the low number of churn in turnover, why ? **3 reasons** 
+We can notice the high number of churn clubs and the low number of churn in turnover, why ? **3 reasons** :
 
-    The churned clubs resulted in a loss of **2100,90 €** in 2024. 
+The churned clubs resulted in a loss of **2100,90 €** in 2024. 
 
-    The upsell on renewed clubs was **1109,54 €** in 2024
+The upsell on renewed clubs was **1109,54 €** in 2024
 
-    1 monthly reacquired plan for the amount of **36,00 €**
+1 monthly reacquired plan for the amount of **36,00 €**
 
-    The net churn in turnover is therefore **955,36 €**
+The net churn in turnover is therefore **955,36 €**
 '''
 st.write(multi)
