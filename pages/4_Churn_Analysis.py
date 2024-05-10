e0564764d1cdf380d94c3bfbfae7a6e3dd4b684c
flabmax <maxence.flaba@sporteasy.net>
2024-05-10 14:13:53 +0200

    button

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -115,6 +115,6 @@ We can notice the high number of churn clubs and the low number of churn in turn
 
     3/ 1 monthly reacquired plan for the amount of 36,00 €
 
---> The net churn in turnover is therefore **955,36 €**
+:warning: The net churn in turnover is therefore **955,36 €**
 '''
 st.write(multi)
