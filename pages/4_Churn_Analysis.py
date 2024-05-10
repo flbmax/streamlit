df0d8af512e9f93a49f4e24436824dec1cd396ce
flabmax <maxence.flaba@sporteasy.net>
2024-05-10 15:52:23 +0200

    button

--- pages/4_Churn_Analysis.py
+++ pages/4_Churn_Analysis.py
@@ -9,6 +9,24 @@ st.markdown('2024-05-21')
 st.header('4. Churn Analysis')
 st.caption('by country from 01/01/2019 to today')
 
+with st.container():
+    col1, col2, col3 = st.columns(3,gap='small')
+    with col1:
+        st.caption("Number of Churn")
+        components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=ztXWgsj&theme=horizon&opt=nointeraction,noselections&select=$::club.country_id,ES,GB,IT,NL,PT",
+            height=100,
+            width=170)
+    with col2:
+        st.caption("2024 Loss")
+        components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=huvHjt&theme=horizon&opt=nointeraction,noselections&select=$::club.country_id,ES,GB,IT,NL,PT" ,
+            height=100,
+            width=220)
+    with col3:
+        st.caption("Total Loss")
+        components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=WNjgyN&theme=horizon&opt=nointeraction,noselections&select=$::club.country_id,ES,GB,IT,NL,PT",
+            height=100,
+            width=250)
+
 st.subheader(':green[a. Targets & Results]')
 with st.container():
     components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=hJUtP&theme=horizon&opt=ctxmenu",
