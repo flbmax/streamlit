ea424d9378ba67859d7c46de5a107b0375b63046
flabmax <maxence.flaba@sporteasy.net>
2024-05-13 12:24:50 +0200

    growth

--- pages/2_Typology_of_teams.py
+++ pages/2_Typology_of_teams.py
@@ -16,7 +16,7 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-
 
 st.subheader(':green[b. Sport]')
 # sport pie chart
-with st.cotainer():
+with st.container():
         components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu",
                 height=500,
                 width=1700)
