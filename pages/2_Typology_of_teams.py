cbec6e39fec99fbead7816aa0b16791c61dc7325
flabmax <maxence.flaba@sporteasy.net>
2024-05-13 12:24:35 +0200

    growth

--- pages/2_Typology_of_teams.py
+++ pages/2_Typology_of_teams.py
@@ -16,9 +16,14 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-
 
 st.subheader(':green[b. Sport]')
 # sport pie chart
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu" ,
-    height=500,
-    width=1700)
+with st.cotainer():
+        components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu",
+                height=500,
+                width=1700)
+        components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=XMCAHYr&theme=horizon&opt=ctxmenu",
+                height=500,
+                width=500)
+        st.caption('BE = Flanders, only dutch speaking part')
 
 
 st.subheader(':green[c. First Source]')
