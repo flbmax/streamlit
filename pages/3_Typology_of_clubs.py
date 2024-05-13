69aba8442796a80087a3cd41b21270f856a3d518
flabmax <maxence.flaba@sporteasy.net>
2024-05-13 14:41:40 +0200

    growth

--- pages/3_Typology_of_clubs.py
+++ pages/3_Typology_of_clubs.py
@@ -64,9 +64,14 @@ with st.container():
 
 st.subheader(':green[e. Clubs Region]')
 # map of the clubs
-components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=CJLgCDy&theme=horizon&opt=ctxmenu",
-    height=600,
-    width=2000)
+with st.container():
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=CJLgCDy&theme=horizon&opt=ctxmenu",
+        height=600,
+        width=2000)
+    components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-84e2-4ef7-90c5-9a809d81bfc5&obj=aMdwJE&theme=horizon&opt=ctxmenu",
+        height=600,
+        width=600)
+
 
 st.subheader(':green[f. Health Score]')
 # clubs health score
