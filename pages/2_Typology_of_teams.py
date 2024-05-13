8f8759c0a625e3f5d5a13542ad1a303eaecb1d22
flabmax <maxence.flaba@sporteasy.net>
2024-05-13 12:22:09 +0200

    growth

--- pages/2_Typology_of_teams.py
+++ pages/2_Typology_of_teams.py
@@ -16,7 +16,7 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-
 
 st.subheader(':green[b. Sport]')
 # sport pie chart
-components.iframe( "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu",
+components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=qAZaPTn&theme=horizon&opt=ctxmenu" ,
     height=500,
     width=1700)
 
@@ -25,8 +25,8 @@ st.subheader(':green[c. First Source]')
 # first source pie chart
 with st.container():
         components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=ssTyVB&theme=horizon&opt=ctxmenu",
-                height=500,
-                width=1700)
+                height=700,
+                width=1000)
         components.iframe( "https://sporteasy-bi.eu.qlikcloud.com/single/?appid=fe30758a-8efe-48e4-943d-367693619486&obj=sPTEbw&theme=horizon&opt=ctxmenu",
                 height=500,
                 width=500)
