78fba7bf52770f97d55e6203328411a462d04960
flabmax <maxence.flaba@sporteasy.net>
2024-05-13 17:33:43 +0200

    growth

--- Global_growth_figures.py
+++ Global_growth_figures.py
@@ -17,8 +17,9 @@ components.iframe("https://sporteasy-bi.eu.qlikcloud.com/single/?appid=dfbf997f-
         width=1000)
 st.caption('*using profile.default_language to determine the country : "es","en","it","nl","pt"')
 rq= '''
-       1. International follow the FR trend (+78% vs +19% for FR) but don't activate his profiles (-3,3% vs +12,5%)
-                -> Activated = becoming an user
+       Remarks:
+        -> International follow the FR trend (+78% vs +19% for FR) but don't activate his profiles (-3,3% vs +12,5%)
+        -> Activated = becoming an user
 '''
 st.write(rq)
 
