05e1c61df99b7535273a29ca553cd98693fcf5a5
flabmax <maxence.flaba@sporteasy.net>
2024-05-21 12:06:58 +0200

    new version

--- Global_growth_figures.py
+++ Global_growth_figures.py
@@ -48,6 +48,15 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+    )
+
 
 st.write('**France Analysis**')
 df_profiles_france = pd.DataFrame()
