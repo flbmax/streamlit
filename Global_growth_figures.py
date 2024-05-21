0ff5c18a9a06d796192b3085b88e81a30201a855
flabmax <maxence.flaba@sporteasy.net>
2024-05-21 12:35:08 +0200

    new version

--- Global_growth_figures.py
+++ Global_growth_figures.py
@@ -74,9 +74,9 @@ st.markdown(
     <div style="display: compact; flex-direction: column; justify-content: center; height: 20vh;">
         <div style="background-color:rgb(237, 240, 243); padding: 20px; border-radius: 20px;">
                 <p>- Seasonnality is exactly the same
-                <p>- International MAU goes from 100k -> 130k -> 150k i.e. +30% -> +15%
-                <p>- Where France MAU goes from 400k -> 550k -> 720k i.e. +38% -> +31%
-                <p>- International MAU growth <b>should be more stable</b></p>
+                <br>- International MAU goes from 100k -> 130k -> 150k i.e. +30% -> +15%
+                <br>- Where France MAU goes from 400k -> 550k -> 720k i.e. +38% -> +31%
+                <br>- International MAU growth <b>should be more stable</b></p>
         </div>
     </div>""",
     unsafe_allow_html=True
