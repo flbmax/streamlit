1df8f7aedc7cb23bbf4ba0b614bf070b34c6c26d
FlabMax <167072324+flbmax@users.noreply.github.com>
2025-02-05 18:03:07 +0100

    Update Global_growth_figures.py
    test balloons

--- Global_growth_figures.py
+++ Global_growth_figures.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 
+st.balloons()
+
 st.set_page_config(page_title="Workshop International", page_icon='sporteasy_logo.png', layout="wide")
 
 
